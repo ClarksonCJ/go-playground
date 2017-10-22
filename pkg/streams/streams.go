9adf7fe71fdb83b658a4c6889fa92169c96828cb
Chris Clarkson <chris.clarkson@cjcsoftwaresolutions.co.uk>
2017-10-22 19:05:46 +0100

    commetns

--- pkg/streams/streams.go
+++ pkg/streams/streams.go
@@ -2,10 +2,10 @@ package streams
 
 import "sync"
 
+//Streams struct to attached Methods to
 type Streams struct{}
 
-/// CreateIntStream converts a slice of integers to a channel of integers
-/// Representative of a Stream in Java
+//CreateIntStream converts a slice of integers to a channel of integers, Representative of a Stream in Java
 func (s *Streams) CreateIntStream(input []int) <-chan int {
 	out := make(chan int, len(input))
 	wg := &sync.WaitGroup{}
@@ -21,6 +21,7 @@ func (s *Streams) CreateIntStream(input []int) <-chan int {
 	return out
 }
 
+//CreateStringStream converts a slice of strings to a channel of strings, Representative of a Stream in Java
 func (s *Streams) CreateStringStream(input []string) <-chan string {
 	out := make(chan string, len(input))
 	wg := &sync.WaitGroup{}
@@ -36,6 +37,7 @@ func (s *Streams) CreateStringStream(input []string) <-chan string {
 	return out
 }
 
+//CreateInterfaceStream converts a slice of interfaces to a channel of interfaces, Representative of a Stream in Java
 func (s *Streams) CreateInterfaceStream(input []interface{}) <-chan interface{} {
 	out := make(chan interface{}, len(input))
 	wg := &sync.WaitGroup{}
